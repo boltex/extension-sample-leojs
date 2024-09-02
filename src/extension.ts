4a77a4387aee70479a588181209995e451df3638
Félix <felixgraphx@gmail.com>
2024-09-02 19:56:10 -0400

    Cleaned up the demo code and its comments.

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "extension-sample-leojs" is now active!');
+
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		void accessOtherExtension();
-		// Display a message box to the user
+
+		void accessLeoJSDemo();
+
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-async function accessOtherExtension() {
-	const extensionId = 'boltex.leojs'; // Replace with the actual ID
-	const extension = vscode.extensions.getExtension(extensionId);
 
+async function accessLeoJSDemo() {
+
+	// Here is how to get the LeoJS extension and its 'g' global object.
+	// It could be done in 'activate', or any other part of the extension's code.
+
+	const extension = vscode.extensions.getExtension('boltex.leojs');
 	if (!extension) {
-		console.error(`Extension ${extensionId} not found.`);
+		console.error(`LeoJS not found.`);
 		return;
 	}
 
-	// Ensure the extension is activated
 	try {
 		const g = await extension.activate();
-		// Now you can use the exported API
+
+		// Now you can use the 'g' leoGlobals object. E.g. to list all its members:
 		// console.log('Accessed exported API:', Object.keys(g).join(' '));
+
 		if (g && g.app && g.app.windowList.length) {
 			const c = g.app.windowList[g.app.gui.frameIndex].c;
 			vscode.window.showInformationMessage(
@@ -46,6 +46,6 @@ async function accessOtherExtension() {
 		}
 
 	} catch (error) {
-		console.error('Error activating extension:', error);
+		console.error('Error activating LeoJS:', error);
 	}
 }
