d59d598f03ba322db2f5abdc8d5b401621086631
Félix <felixgraphx@gmail.com>
2024-09-22 22:08:33 -0400

    Added example of how to 'register' a Leo hook event with g.registerHandler.

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-# ![LeoEditor](https://raw.githubusercontent.com/boltex/leojs/master/resources/leoapp.png) LeoJS API Access Sample Extension
+# LeoJS API Access Sample Extension
 
-> *For Leo script samples with LeoJS and UI interaction, see [scripting-samples-leojs](https://github.com/boltex/scripting-samples-leojs)*
+> _For Leo script samples with LeoJS and UI interaction, see [scripting-samples-leojs](https://github.com/boltex/scripting-samples-leojs)_
 
-### This is a sample extension that shows you how to access and use LeoJS from another extension through its exposed instance of the leoGlobals 'g' object.
+**This is a sample extension that shows you how to access and use LeoJS from another extension through its exposed instance of the leoGlobals 'g' object.**
 
 It grabs the extension, and the g object is what is actually exported by the activation method
+
 ```
 const extension = vscode.extensions.getExtension('boltex.leojs');
-const g = await extension.activate()
+const g = await extension.activate();
 ```
 
 To run and try it. with LeoJS installed, and this extension started (see Running the Sample below) Open the command palette with `CTRL+SHIFT+P` and type 'Hello'.
 
-This should access LsoJs and display a message about the number of Leo documents opened along with the headline of the currently selected node.
+This should access LeoJs and display a message about the number of Leo documents opened along with the headline of the currently selected node.
+
+There is also a hook that is setup as example of usage of Leo 'hooks'with g.registerHandler. Inserting a node triggers a hook that is registered on the "create-node" event like so:
+
+```
+g.registerHandler("create-node", newNodeHandler);
+```
 
 ## Demo
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,40 @@
 import * as vscode from 'vscode';
 
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext): Promise<unknown> {
 
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extension-sample-leojs" is now active!');
+	vscode.window.showInformationMessage('Congratulations, your extension "extension-sample-leojs" is now active!');
+
+	// Here is how to get the LeoJS extension and its 'g' global object.
+	// It could be done here in the 'activate'function,  and stored in a variable,
+	// or in any other part of the extension's code.
+	const extension = vscode.extensions.getExtension('boltex.leojs');
+	if (!extension) {
+		vscode.window.showErrorMessage(`LeoJS not found.`);
+		return;
+	}
+
+	try {
+		const g = await extension.activate();
+		// Now you can use the 'g' leoGlobals object. E.g. to list all its members:
+		// console.log('Accessed exported API:', Object.keys(g).join(' '));
+
+		// Here's how to use a hook and registerHandler to react to the user inserting new nodes
+		const newNodeHandler = (tag: string, keywords: Record<string, any>) => {
+			// In a timeout for the user's headline input window to resolve.
+			setTimeout(() => { vscode.window.showInformationMessage(`Handler ${tag}: New node is ${keywords.p.h}`); }, 0);
+		};
+
+		g.registerHandler("create-node", newNodeHandler);
+
+	} catch (error) {
+		vscode.window.showErrorMessage('Error activating LeoJS');
+	}
 
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-
-		void accessLeoJSDemo();
-
-		vscode.window.showInformationMessage('Hello World!');
+		void accessLeoJSDemo(); // This command also shows how to get the g global object to access c.p, etc.
 	});
 
 	context.subscriptions.push(disposable);
@@ -20,12 +43,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 async function accessLeoJSDemo() {
 
-	// Here is how to get the LeoJS extension and its 'g' global object.
-	// It could be done in 'activate', or any other part of the extension's code.
-
+	// Here is again how to get the LeoJS extension and its 'g' global object. (if not already available in some stored variable)
+	// It could be done above in the 'activate', or any other part of the extension's code.
 	const extension = vscode.extensions.getExtension('boltex.leojs');
 	if (!extension) {
-		console.error(`LeoJS not found.`);
+		vscode.window.showErrorMessage(`LeoJS not found.`);
 		return;
 	}
 
@@ -34,7 +56,6 @@ async function accessLeoJSDemo() {
 
 		// Now you can use the 'g' leoGlobals object. E.g. to list all its members:
 		// console.log('Accessed exported API:', Object.keys(g).join(' '));
-
 		if (g && g.app && g.app.windowList.length) {
 			const c = g.app.windowList[g.app.gui.frameIndex].c;
 			vscode.window.showInformationMessage(
@@ -46,6 +67,6 @@ async function accessLeoJSDemo() {
 		}
 
 	} catch (error) {
-		console.error('Error activating LeoJS:', error);
+		vscode.window.showErrorMessage('Error activating LeoJS');
 	}
 }
